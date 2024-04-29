94ccde37bba5aa2963a6a49b6f01c45220e42fba
coffee2think <135593667+coffee2think@users.noreply.github.com>
2024-04-29 23:47:41 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 79.68 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -1,16 +1,29 @@
-// 다른 사람 풀이 성능 비교
-import java.util.*;
+import java.util.Set;
+import java.util.HashSet;
 
 class Solution {
     public int solution(int[] elements) {
         Set<Integer> set = new HashSet<>();
-        int[] dp = new int[elements.length];
-        for(int len = 1;len <= elements.length; len++){
-            for(int i = 0;i<elements.length;i++){
-                dp[i] += elements[(len+i-1)%elements.length];
-                set.add(dp[i]);
+        int n = elements.length;
+
+        // 누적합 배열 초기화
+        int[] psum = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            psum[i + 1] = psum[i] + elements[i];
+        }
+
+        for (int len = 1; len <= n; len++) {
+            for (int start = 0; start < n; start++) {
+                // 범위를 초과한다면 전체에서 포함되지 않는 수열의 합을 빼고,
+                // 범위를 벗어나지 않는다면 포함되는 수열의 합을 계산하여 집합에 추가함
+                if (start + len - 1 >= n) {
+                    set.add(psum[n] - (psum[start] - psum[start - (n - len)]));
+                } else {
+                    set.add(psum[start + len] - psum[start]);
+                }
             }
         }
+
         return set.size();
     }
 }
