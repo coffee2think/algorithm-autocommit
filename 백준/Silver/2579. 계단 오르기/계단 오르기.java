d60abb22582f7b073c8215f617f928a22e8b68a5
coffee2think <135593667+coffee2think@users.noreply.github.com>
2024-05-05 00:55:49 +0900

    [Silver III] Title: 계단 오르기, Time: 124 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] stairs = new int[N + 1];
+        int[] dp = new int[stairs.length];
+        for (int i = 1; i < stairs.length; i++) {
+            stairs[i] = Integer.parseInt(br.readLine());
+        }
+
+        dp[1] = stairs[1];
+        if(N > 1) dp[2] = dp[1] + stairs[2];
+        for (int i = 3; i < stairs.length; i++) {
+            dp[i] += Math.max(dp[i - 2], dp[i - 3] + stairs[i - 1]) + stairs[i];
+        }
+
+        System.out.println(dp[N]);
+        br.close();
+    }
+}
