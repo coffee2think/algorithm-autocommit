83649c3e33371fb1620700c6a738541fc6ed76e5
coffee2think <135593667+coffee2think@users.noreply.github.com>
2024-05-05 20:47:39 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 120 ms, Memory: 14088 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,49 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
 
 public class Main {
-    static int[] cases = new int[11];
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        init();
+        // init dp array
+        int[] dp = new int[11];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        for (int i = 4; i < dp.length; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
 
+        // input
         int T = Integer.parseInt(br.readLine());
         for (int t = 0; t < T; t++) {
             int n = Integer.parseInt(br.readLine());
-            sb.append(cases[n]).append("\n");
+            sb.append(dp[n]).append("\n");
         }
 
+        // result
         System.out.println(sb);
         br.close();
-    }
-
-    public static void init() {
-        for (int n = 1; n <= 10; n++) {
-            cases[n] = partitioning(n);
-        }
-    }
-
-    public static int partitioning(int n) {
-        return partitioning(n, new Stack<>());
-    }
-
-    public static int partitioning(int n, Stack<Integer> stack) {
-        if (n < 1) {
-            return 1;
-        }
-
-        int count = 0;
-        for (int i = 1; i <= Math.min(3, n); i++) {
-            stack.push(i);
-            count += partitioning(n - i, stack);
-            stack.pop();
-        }
-
-        return count;
     }
 }
