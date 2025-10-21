dd0c822be21d844d7660d84786280b077ed67092
coffee2think <135593667+coffee2think@users.noreply.github.com>
2025-10-22 00:07:20 +0900

    [Silver III] Title: 바이너리 왕국, Time: 848 ms, Memory: 227264 KB -BaekjoonHub

--- 백준/Silver/16567. 바이너리 왕국/바이너리 왕국.java
+++ 백준/Silver/16567. 바이너리 왕국/바이너리 왕국.java
@@ -13,25 +13,16 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         boolean[] road = new boolean[N];
+
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
+        road[0] = Integer.parseInt(st.nextToken()) == 0 ? false : true;
+        int flipCount = road[0] ? 1 : 0;
+        for (int i = 1; i < N; i++) {
             road[i] = Integer.parseInt(st.nextToken()) == 0 ? false : true; // false: 깨끗, true: 오염
-        }
-
-        int[] dp = new int[M + 1];
-        int dpIndex = 0;
-
-        boolean flag = false;
-        int flipCount = 0;
-        for (int i = 0; i < N; i++) {
-            if (road[i] && !flag) {
-                flag = true;
+            if (!road[i - 1] && road[i]) {
                 flipCount++;
-            } else if (!road[i] && flag) {
-                flag = false;
             }
         }
-        dp[0] = flipCount;
 
         // when
         for (int m = 0; m < M; m++) {
@@ -39,35 +30,25 @@ public class Main {
             int cmd = Integer.parseInt(st.nextToken());
 
             if (cmd == 0) {
-                sb.append(dp[dpIndex]).append("\n");
+                sb.append(flipCount).append("\n");
                 continue;
             }
 
-            // cmd == 1
             int target = Integer.parseInt(st.nextToken()) - 1;
 
-            // 이미 오염된 땅이면 변화 없음
             if (road[target]) {
                 continue;
             }
 
             road[target] = true;
+            flipCount++;
 
-            // 타겟이 양 끝이라면,
-            // 이웃하는 땅의 상태가 오염되었다면 유지, 아니라면 증가
-            if (target == 0 || target == N - 1) {
-                boolean neighbor = road[Math.abs(target - 1)];
-                dp[++dpIndex] = dp[dpIndex - 1] + (neighbor ? 0 : 1);
-                continue;
+            if (target > 0 && road[target - 1] && road[target]) {
+                flipCount--;
             }
 
-            // 000 → 010 : flipCount 1개 증가
-            // 101 → 111 : flipCount 1개 감소
-            // 100 → 110, 001 → 011 : 변화 없음
-            if (!road[target - 1] && !road[target + 1]) {
-                dp[++dpIndex] = dp[dpIndex - 1] + 1;
-            } else if (road[target - 1] && road[target + 1]) {
-                dp[++dpIndex] = dp[dpIndex - 1] - 1;
+            if (target < N - 1 && road[target] && road[target + 1]) {
+                flipCount--;
             }
         }
 
